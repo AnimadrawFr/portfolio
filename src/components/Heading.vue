efacd66ec6e6b82c2d63f33b0798b3be7b50421a
Piersanti Dylan <dylngae@outlook.com>
2021-03-14 11:37:01 +0100

    first commit

---
+++ src/components/Heading.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="heading" class="panel">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+@import "./src/styles/global.scss";
+
+</style>
